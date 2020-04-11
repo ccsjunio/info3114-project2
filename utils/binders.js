a27e7e0ad5875cc517c126fb2f654bbb79821ad8
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-04-11 13:41:43 -0400

    binders.js docummented

--- utils/binders.js
+++ utils/binders.js
@@ -3,14 +3,25 @@ import { newDeviceToNamespace } from "/utils/add-device.js";
 import { updateDeviceInNamespace } from "/utils/update-device.js";
 import { deleteDeviceFromNamespace } from "/utils/delete-device.js";
 
+// this function add listeners to all fields
+// available in the pages. it listen to changes
+// in the contents or state of the field
 let bindFieldsToChangeEvent = function(){
   //bind each device field in order to enable update button on changing events
   let fields = document.querySelectorAll(".deviceField");
   fields.forEach((field)=>{
-      //console.log("this field is binding to change *************************", field);
+      // identify the namespace of the field
       let namespace = field.getAttribute("namespace");
+      // identify the button update of this namespace
       let button = document.querySelector(`.updateDeviceButton[namespace="${namespace}"]`);
-      if(button.disabled=true){
+      // if the button update is disabled
+      // add listeners to the field for
+      // changing the content of the field and exiting the field
+      // changing the contents of the field while still in the field
+      // by releasing a key while focusing in the field
+      // the listener points to a callback function to enable
+      // the update button
+      if(button.disabled==true){
           button.setAttribute("toBeEnabled","true");
           field.addEventListener("change",enableUpdateButton);
           field.addEventListener("blur",enableUpdateButton);
@@ -18,11 +29,16 @@ let bindFieldsToChangeEvent = function(){
       }
   }); // end of fields.forEach field
 
+  // function to enable the update button
   function enableUpdateButton(event){
-      //console.log("enableUpdateButton called");
+      // get which field was changed
       let field = event.target;
+      // get the namespace were the device containing this field is
       let namespace = field.getAttribute("namespace");
+      // get the update button
       let button = document.querySelector(`.updateDeviceButton[toBeEnabled="true"][namespace="${namespace}"]`);
+      // if the button is defined, enable it and remove the flag
+      // "to be enabled"
       if(button!==undefined && button!==null){
           button.disabled = false;
           button.removeAttribute("toBeEnabled");
@@ -30,22 +46,25 @@ let bindFieldsToChangeEvent = function(){
   }
 } // function bindFieldsToChangeEvent()
 
+// add listeners on all navigation buttons for
+// the click of the user
 let bindNavigationButtons = function(){
   //bind next button
   let nextDeviceButtons = document.querySelectorAll(".nextDeviceButton");
-  //console.log("next device buttons ==================================== ",nextDeviceButtons);
   nextDeviceButtons.forEach((button)=>{
       button.addEventListener("click",showNextDeviceInNamespace);
   });
 
   //bind previous button
   let previousDeviceButtons = document.querySelectorAll(".previousDeviceButton");
-  //console.log("previous device buttons ==================================== ",previousDeviceButtons);
   previousDeviceButtons.forEach((button)=>{
       button.addEventListener("click",showPreviousDeviceInNamespace);
   });
 } // end of bindNavigationButtons
 
+// add listeners to all data manipulation buttons
+// new, update and delete
+// to listen to the click of the user
 let bindDataButtons = function(){
   //bind new button
   let newDeviceButtons = document.querySelectorAll(".newDeviceButton");
